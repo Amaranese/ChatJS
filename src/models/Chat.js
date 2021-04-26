04096d64fc515b901d505715f041bd3f597676f6
Amaranese <alejandro.maranes@asendia.com>
2021-04-26 15:39:57 +0200

    Create Chat.js

---
+++ src/models/Chat.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const ChatSchema = new Schema({
+  nick: String,
+  msg: String,
+  created: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Chat', ChatSchema);
